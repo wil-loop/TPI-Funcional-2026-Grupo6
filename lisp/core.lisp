10297555e1658879ae6842da0ac72e8d43a4122f
Gabriel Fava <gabrielfava1999@gmail.com>
2026-06-05 16:05:27 -0300

    Implementacion requerimiento 2

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -11,14 +11,16 @@
 
 ;; ========================================================
 ;; FUNCIÓN: timer
-;; NATURALEZA: a desarrollar
-;; ESTRATEGIA: a desarrollar
-;; IMPACTO: a desarrollar
+;; NATURALEZA: Pura (Dado un timestamp devuelve siempre el mismo color)
+;; ESTRATEGIA: Evaluacion condicional (Uso de cond. No requiere orden superior ni recursividad de cola)
+;; IMPACTO: No destructiva (No muta estados externos)
 ;; ========================================================
 (defun timer (tiempo-unix)
+  (let ((resto (mod tiempo-unix 216)))
     (cond
-      ((<= (mod tiempo-unix 216) 89)'en-rojo)
-      ((<= (mod tiempo-unix 216) 95)'en-amarillo)
+      ((<= resto 89)'en-rojo)
+      ((<= resto 95)'en-amarillo)
       (t 'en-verde) ;si no se encuentra en ninguno de los anteriores rangos quiere decir que esta en verde.
-      )
     )
+  )
+)

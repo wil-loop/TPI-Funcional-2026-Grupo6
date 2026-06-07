e5f6990c97cf45ed731f59355dc27ce9e0b8762e
Wil-loop <canterowilliam61@gmail.com>
2026-06-06 23:11:58 -0300

    Correccion requerimiento 1

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -1,3 +1,4 @@
+
 	;; ============================================================
 	;; FUNCION: transicion
 	;; NATURALEZA: pura (dado el color actual y al que se desea cambiar retorna una lista con el color actual y la accion a realzar)
@@ -7,9 +8,9 @@
 
 	(defun transicion (color-actual cambiar-a)
 		(cond 
-			((and(equal color-actual 'en-rojo) (equal cambiar-a 'amarillo)) '('en-rojo 'cambiar-a-amarillo) ) 
-			((and(equal color-actual 'en-amarillo) (equal cambiar-a 'verde) ) '('en-amarillo 'cambiar-a-verde)) 
-			((and(equal color-actual 'en-verde) (equal cambiar-a 'rojo) ) '('en-verde 'cambiar-a-rojo) ) 
+			((and(equal color-actual 'en-rojo) (equal cambiar-a 'amarillo)) '('en-rojo "cambiar-a-amarillo")) 
+			((and(equal color-actual 'en-amarillo) (equal cambiar-a 'verde) ) '('en-amarillo "cambiar-a-verde")) 
+			((and(equal color-actual 'en-verde) (equal cambiar-a 'rojo) ) '('en-verde "cambiar-a-rojo")) 
 			(t (list color-actual 'accion-por-defecto))
 			)
 		)

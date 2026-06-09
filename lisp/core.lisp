996af9bfba6f33a199bc3f346fe09702b21ae6bd
Wil-loop <canterowilliam61@gmail.com>
2026-06-09 20:21:19 -0300

    restaura requerimiento 5

--- lisp/core.lisp
+++ lisp/core.lisp
@@ -71,3 +71,14 @@
 		(t
 			"Ciclo en rango óptimo")
 	)	
+
+;; REQUERIMIENTO 5
+;; ============================================================
+;; FUNCION: ciclos-por-tiempo
+;; NATURALEZA: Pura (Dado un argumento, siempre le corresponde el mismo resultado)
+;; ESTRATEGIA:  Función aritmetica simple / Combinación de operaciones
+;; IMPACTO: no destructiva
+;; ============================================================ 
+(defun ciclos-por-tiempo (minutos)
+  	(nth-value 0(floor (* minutos 60) 216))
+  )

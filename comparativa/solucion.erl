a16a640432203997bf46de15e89a2d66c8ab32f2
TheGabo1999 <146967310+TheGabo1999@users.noreply.github.com>
2026-06-14 13:41:09 -0300

    actualizo Fase 3 con la funcion timer adaptada a Erlang

--- comparativa/solucion.erl
+++ comparativa/solucion.erl
@@ -21,3 +21,21 @@ transicion(ColorActual, _CambiarA) when ColorActual =:= "en-verde", _CambiarA =:
 transicion(ColorActual, _CambiarA) ->     
     [ColorActual, "accion-por-defecto"]. % finalizar con "." --> finaliza la funcion
 
+%	;; ============================================================
+%	;; FUNCIÓN: timer
+%	;; NATURALEZA: Pura (Dado un timestamp devuelve siempre el mismo átomo)
+%	;; ESTRATEGIA: Evaluación condicional (Uso de if para emular el cond lógico).
+%	;; IMPACTO: No destructiva (No muta estados externos, Erlang impone inmutabilidad)
+%	;; ============================================================
+
+timer(TiempoUnix) ->
+    Resto = TiempoUnix rem 225, % con una "," indicamos que la ejecución continúa en la siguiente instrucción.
+    % se usa rem ya que Erlang no maneja la funcion mod. Además, aqui vinculamos directamente (pattern matching) el valor a la variable Resto.
+    if % funciona de manera casi identica a la estructura cond, evalúa las condiciones en orden y se detiene en la primera verdadera.
+        Resto =< 89  -> 'en-rojo'; % ";" indica fin de una clausula.
+        Resto =< 92  -> 'en-rojo-intermitente';
+        Resto =< 212 -> 'en-verde';
+        Resto =< 215 -> 'en-verde-intermitente';
+        Resto =< 221 -> 'en-amarillo';
+        true         -> 'en-amarillo-intermitente' % true es el equivalente a t en Lisp.
+    end. % "." indica fin de la función.
